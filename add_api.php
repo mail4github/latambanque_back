34a47706f2ba3872b502d079996a5276cc3968f1
Pavel J <mail4github@advertising-page.com>
2026-08-16 20:26:12 +0300

    minor changes

--- add_api.php
+++ add_api.php
@@ -3,44 +3,44 @@ require_once(DIR_WS_CLASSES.'send_mail.class.php');
 
 if ( !empty($_GET['custom_api']) ) {
 
-    // >>>>>>>>>>>>>>>>>>>>>	
-    // Initiate $user_obj
-    // >>>>>>>>>>>>>>>>>>>>>
-    
-    $user_obj = new User();
-    $user_obj->userid = 0;
-    if (!empty($_POST['bot_userid'])) {
-        $user_obj->userid = (int)$_POST['bot_userid'];
-    }
-    else
-    if (!empty($_POST['userid'])) {
-        $user_obj->userid = (int)$_POST['userid'];
-    }
-    $user_obj->email = tep_sanitize_string($_POST['email']);
+	// >>>>>>>>>>>>>>>>>>>>>	
+	// Initiate $user_obj
+	// >>>>>>>>>>>>>>>>>>>>>
+	
+	$user_obj = new User();
+	$user_obj->userid = 0;
+	if (!empty($_POST['bot_userid'])) {
+		$user_obj->userid = (int)$_POST['bot_userid'];
+	}
+	else
+	if (!empty($_POST['userid'])) {
+		$user_obj->userid = (int)$_POST['userid'];
+	}
+	$user_obj->email = tep_sanitize_string($_POST['email']);
 
-    if ( !empty($_POST['token']) && $user_obj->read_data(false) ) {
-        $user_obj->token_valid = check_token($user_obj);
-    }
-    
-    /**
-     * Get crypto address to deposit funds.
-     *
-     * @Route("/api/custom_api/", name="get_crypto_addr", methods={"POST"})
-     *
-     * @param custom_command            (required)  get_crypto_addr
+	if ( !empty($_POST['token']) && $user_obj->read_data(false) ) {
+		$user_obj->token_valid = check_token($user_obj);
+	}
+	
+	/**
+	 * Get crypto address to deposit funds.
+	 *
+	 * @Route("/api/custom_api/", name="get_crypto_addr", methods={"POST"})
+	 *
+	 * @param custom_command            (required)  get_crypto_addr
 	 * @param currency					(required)	'btc' | 'usdt'
-     *
-     * @return Json string
-     */
-    if ( @$_POST['custom_command'] == 'get_crypto_addr' ) {
+	 *
+	 * @return Json string
+	 */
+	if ( @$_POST['custom_command'] == 'get_crypto_addr' ) {
 
-        if ( empty($_POST['currency']) ) {
-            echo generate_answer(0, 'currency is empty' , '', 'ERROR_EMPTY_CURRENCY');
-            exit;
-        }
+		if ( empty($_POST['currency']) ) {
+			echo generate_answer(0, 'currency is empty' , '', 'ERROR_EMPTY_CURRENCY');
+			exit;
+		}
 
-        // Getting address from DB first
-        $make_post_request = false;
+		// Getting address from DB first
+		$make_post_request = false;
 		$res = $user_obj->get_request_to_add_funds(
 			0, 
 			$_POST['currency'], 
@@ -66,96 +66,178 @@ if ( !empty($_GET['custom_api']) ) {
 				'update', 'userid = "'.$user_obj->userid.'"'
 			);
 			echo generate_answer(1, '', ['address' => $res['pay_email']]);
-        	exit;
+			exit;
 		}
 
-        // if there is no saved address then get address from third party source
-        $currency = new Currency();
-        $currency->read_data($_POST['currency']);
-        $id_account = '20'.$user_obj->userid;
-        $result_address = '';
-        $result_error = '';
-        // Getting a PHP code from DB. This code is receiving address from a third party API
-        $eval_code = $currency->blocks_explorer;
-        $currency_code = strtoupper($_POST['currency']);
-        $debug_info = '';
+		// if there is no saved address then get address from third party source
+		$currency = new Currency();
+		$currency->read_data($_POST['currency']);
+		$id_account = '20'.$user_obj->userid;
+		$result_address = '';
+		$result_error = '';
+		// Getting a PHP code from DB. This code is receiving address from a third party API
+		$eval_code = $currency->blocks_explorer;
+		$currency_code = strtoupper($_POST['currency']);
+		$debug_info = '';
 
-        if (!empty($eval_code)) {
-            eval($eval_code);
-        }
+		if (!empty($eval_code)) {
+			eval($eval_code);
+		}
 
-        if (!empty($result_address)) {
-            $currency = tep_sanitize_string($_POST['currency'], 10);
-            $invoiceNmb = INVOICE_PREFIX.ADD_FUNDS_PREFIX.USERID_PREFIX.$user_obj->userid.WEBSITE_NUMBER_PREFIX.WEBSITE_NUMBER.WEBSITE_NUMBER_SUFFIX;
-            $invoiceNmb = tep_sanitize_string($invoiceNmb.'_'.date("ymdHis").rand(1, 99), 64);
-            tep_db_perform(TABLE_PAY_ATTEMPTS, [
-                'userid' => $user_obj->userid,
-                'invoice' => $invoiceNmb,
-                'ip' => '',
-                '_created' => 'now()',
-                'country' => '',
-                'total' => 0,
-                'received' => 0,
-                'note' => '',
-                'pay_email' => $result_address,
-                'priv_key' => 'none',
-                'payment_method' => $currency,
-                'currency' => strtoupper($currency),
-                'total_in_currency' => 0,
-                'serviceid' => 0,
-                'siteid' => '',
-            ]);
+		if (!empty($result_address)) {
+			$currency = tep_sanitize_string($_POST['currency'], 10);
+			$invoiceNmb = INVOICE_PREFIX.ADD_FUNDS_PREFIX.USERID_PREFIX.$user_obj->userid.WEBSITE_NUMBER_PREFIX.WEBSITE_NUMBER.WEBSITE_NUMBER_SUFFIX;
+			$invoiceNmb = tep_sanitize_string($invoiceNmb.'_'.date("ymdHis").rand(1, 99), 64);
+			tep_db_perform(TABLE_PAY_ATTEMPTS, [
+				'userid' => $user_obj->userid,
+				'invoice' => $invoiceNmb,
+				'ip' => '',
+				'_created' => 'now()',
+				'country' => '',
+				'total' => 0,
+				'received' => 0,
+				'note' => '',
+				'pay_email' => $result_address,
+				'priv_key' => 'none',
+				'payment_method' => $currency,
+				'currency' => strtoupper($currency),
+				'total_in_currency' => 0,
+				'serviceid' => 0,
+				'siteid' => '',
+			]);
 			echo generate_answer(1, '', ['address' => $result_address, 'id_account' => $id_account]);
-        	exit;
-        }
-        if (!empty($result_error)) {
-            echo generate_answer(0, $result_error);
-        }
-        else {
-            echo generate_answer(0, 'Error: no answer from API');
-        }
-        exit;
-    }
-    else
-    /**
-     * Send notification to user.
-     *
-     * @Route("/api/custom_api/", name="send_notification", methods={"POST"})
-     *
-     * @param custom_command            (required)  send_notification
-     * @param for_userid                (required)	str
+			exit;
+		}
+		if (!empty($result_error)) {
+			echo generate_answer(0, $result_error);
+		}
+		else {
+			echo generate_answer(0, 'Error: no answer from API');
+		}
+		exit;
+	}
+	else
+	/**
+	 * Send notification to user.
+	 *
+	 * @Route("/api/custom_api/", name="send_notification", methods={"POST"})
+	 *
+	 * @param custom_command            (required)  send_notification
+	 * @param for_userid                (required)	str
 	 * @param subject					(required)	str
-     * @param message					(required)	str
-     *
-     * @return Json string
-     */
-    if ( @$_POST['custom_command'] == 'send_notification' ) {
+	 * @param message					(required)	str
+	 *
+	 * @return Json string
+	 */
+	if ( @$_POST['custom_command'] == 'send_notification' ) {
 
-        check_credentials(PERMISSION_MANAGER, $user_obj);
+		check_credentials(PERMISSION_MANAGER, $user_obj);
 
-        if ( empty($_POST['for_userid']) ) {
-            echo generate_answer(0, 'for_userid is empty' , '', 'ERROR_EMPTY_FOR_USERID');
-            exit;
-        }
-        if ( empty($_POST['subject']) ) {
-            echo generate_answer(0, 'subject is empty' , '', 'ERROR_EMPTY_SUBJECT');
-            exit;
-        }
-        if ( empty($_POST['message']) ) {
-            echo generate_answer(0, 'message is empty' , '', 'ERROR_EMPTY_MESSAGE');
-            exit;
-        }
+		if ( empty($_POST['for_userid']) ) {
+			echo generate_answer(0, 'for_userid is empty' , '', 'ERROR_EMPTY_FOR_USERID');
+			exit;
+		}
+		if ( empty($_POST['subject']) ) {
+			echo generate_answer(0, 'subject is empty' , '', 'ERROR_EMPTY_SUBJECT');
+			exit;
+		}
+		if ( empty($_POST['message']) ) {
+			echo generate_answer(0, 'message is empty' , '', 'ERROR_EMPTY_MESSAGE');
+			exit;
+		}
 
-        $mail = new send_mail();
+		$mail = new send_mail();
 		$mail->save_email_to_db(
-            intval($_POST['for_userid']), 
-            tep_sanitize_string($_POST['subject']), // subject
-            tep_sanitize_string($_POST['message']), // html_body
-            $user_obj->userid // sender_userid
-        );
+			intval($_POST['for_userid']), 
+			tep_sanitize_string($_POST['subject']), // subject
+			tep_sanitize_string($_POST['message']), // html_body
+			$user_obj->userid // sender_userid
+		);
 
-        echo generate_answer(1, '', []);
-        exit;
-    }
+		echo generate_answer(1, '', []);
+		exit;
+	}
+	else
+	/**
+	 * Get list of chats by admin
+	 *
+	 * @Route("/api/custom_api/", name="admin_get_chats_list", methods={"POST"})
+	 *
+	 * @param custom_command            (required)  admin_get_chats_list
+	 *
+	 * @return Json string
+	 */
+	if ( @$_POST['custom_command'] == 'admin_get_chats_list' ) {
+
+		check_credentials(PERMISSION_MANAGER, $user_obj);
+
+		$total_unread = 0;
+
+		$q = '
+		SELECT 
+			recepient AS userId,
+			`name`,
+			docNumber,
+			SUM(total_messages) AS total,
+			0 AS unread,
+			lastText,
+			IF(LOCATE(",", sender_permissions) OR LOCATE("MNG", sender_permissions), "support", "user") AS lastFrom,
+			UNIX_TIMESTAMP(created) AS lastUnixDate,
+			DATE_FORMAT(created, "%Y-%m-%dT%H:%i:%s.000Z") AS lastDate
+		FROM (
+			SELECT *,
+				GROUP_CONCAT( permissionid SEPARATOR ",") AS sender_permissions
+			FROM (
+				SELECT 
+					tb2.userid AS sender,
+					tb2.wall_userid AS recepient,
+					tb2.text AS lastText,
+					tb2.created,
+					tb2.total_messages,
+					CONCAT(users.firstname, " ", users.lastname) AS name,
+					users.email AS docNumber,
+					permissionid
+				FROM (
+					SELECT 
+						userid,
+						wall_userid,
+						text,
+						created,
+						tb1.total_messages
+					FROM '.TABLE_FORUM_TOPICS.' AS forum_topics
+					INNER JOIN (
+						SELECT 
+							userid AS tb_userid,
+							MAX(created) AS latest,
+							COUNT(*) AS total_messages,
+                        	wall_userid AS tb_wall_userid
+						FROM '.TABLE_FORUM_TOPICS.' 
+						GROUP BY 
+							wall_userid
+					) AS tb1
+					ON forum_topics.created = tb1.latest AND forum_topics.wall_userid = tb1.tb_wall_userid
+					ORDER BY created DESC
+				) AS tb2 
+				LEFT JOIN '.TABLE_USERS.' AS users ON users.userid = tb2.wall_userid
+				LEFT JOIN '.TABLE_USERS_PERMISSIONS.' AS users_permissions ON users_permissions.userid = tb2.userid
+			) AS tb5
+			GROUP BY sender, recepient
+		) AS tb3
+		GROUP BY recepient 
+		ORDER BY IF(LOCATE(",", sender_permissions) OR LOCATE("MNG", sender_permissions), DATE_SUB(created, INTERVAL 1 YEAR), created) DESC
+		';
+		$res_arr = array();
+		if ( $r = tep_db_query($q) ) {
+			while ( $row = tep_db_fetch_array($r) ) {
+				$res_arr[] = $row;
+				if ( intval($row['unread']) ) {
+					$total_unread++;
+				}
+			}
+		}
+		
+		echo generate_answer(1, '', ['chats' => $res_arr, 'totalUnread' => $total_unread]);
+		exit;
+	}
 }
 ?>
